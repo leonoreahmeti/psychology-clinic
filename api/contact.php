c1d9d3da4a576c492e701936395db975c3a2f0b2
Leonore Ahmeti <leonoreahmeti@gmail.com>
2026-09-07 14:35:41 +0300

    Fix API CORS and booking requests

--- api/contact.php
+++ api/contact.php
@@ -1,9 +1,9 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://leonoreahmeti.github.io');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
@@ -11,8 +11,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 require_once __DIR__ . '/../config/database.php';
-
-
 /*
 |--------------------------------------------------------------------------
 | ONLY POST REQUESTS
